dd12630e6f6c3880465e5f50bfb07708a91d9e58
Sarah Pinkelman <spinkelman@salesforce.com>
2021-09-30 23:11:23 -0500

    Remove Robot files

--- Calendly/robot/Resources/locators.py
+++
@@ -1,6 +0,0 @@
-"tictactoe": {
-        "field": "//input[@placeholder='Enter a number to generate a tic tac toe board'], '{}')]",
-        "button": "//button[@id='start']'{}']",
-        "tablelocationvalue": "//td[@id='{}']/[text='{}']",
-    }
-}
